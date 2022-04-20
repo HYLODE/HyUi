fd9f9142de5fe9b606eb0d1ac382832e1ea597b6
drstevok <doc@steveharris.me>
2022-04-20 06:41:01 +0100

    WIP

--- tests/functional/test_dash_mwe.py
+++ tests/functional/test_dash_mwe.py
@@ -5,10 +5,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
 from . import prepare_app, prepare_page
+# FIXME: tests 2+ fail; something to do with reinstantiating the app?
+# but any single test passes
 
 
 def test_example1(dash_duo):
     prepare_app(dash_duo)
+    prepare_page(dash_duo, 'gapminder')
     dash_duo.wait_for_text_to_equal(
         "#h1-title", "Gapminder example with callbacks", timeout=4)
     dash_duo.wait_for_element("#graph-with-slider", timeout=4)
@@ -16,7 +19,7 @@ def test_example1(dash_duo):
 
 def test_example2(dash_duo):
     prepare_app(dash_duo)
-
+    prepare_page(dash_duo, 'gapminder')
     WebDriverWait(dash_duo.driver, 10).until(
         expected_conditions.visibility_of_element_located(
             (By.CSS_SELECTOR, "#graph-with-slider")))
