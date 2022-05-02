b5162cebc8f7ee7bc287c43df1ef36352b24afca
drstevok <doc@steveharris.me>
2022-05-02 22:10:47 +0100

    getting the python debugger to work

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
+import os
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
-# Code of your application, which uses environment variables (e.g. from `os.environ` or
-# `os.getenv`) as if they came from the actual environment.
-import os
+foo = 'bar'
+
+# Code of your application, which uses environment variables (e.g. from
+# `os.environ` or `os.getenv`) as if they came from the actual environment.
 assert os.getenv('SECRET') == "FALSE"
 
 name = 'steve'
+
 print(f'hello {name}')
 print('foobar')
