64e7ca3a1fa6a46ce5100ac6a769d3c2dea40e69
Steve Harris <doc@steveharris.me>
2022-05-12 23:41:27 +0100

    get and put testing

--- tuts/fastapi_v2/main.py
+++ tuts/fastapi_v2/main.py
@@ -1,11 +1,41 @@
 # https://fastapi.tiangolo.com/tutorial/testing/
 # then drop onto command line in activated virtual env
 # pytest
-from fastapi import FastAPI
+from typing import Optional  # for Python 3.6--3.0
+
+from fastapi import FastAPI, Header, HTTPException
+from pydantic import BaseModel
+
+fake_secret_token = "coneofsilence"
+
+fake_db = {
+    "foo": {"id": "foo", "title": "Foo", "description": "There goes my hero"},
+    "bar": {"id": "bar", "title": "Bar", "description": "The bartenders"},
+}
 
 app = FastAPI()
 
 
-@app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
+class Item(BaseModel):
+    id: str
+    title: str
+    description: Optional[str] = None
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_main(item_id: str, x_token: str = Header(...)):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item_id not in fake_db:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return fake_db[item_id]
+
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item, x_token: str = Header(...)):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item.id in fake_db:
+        raise HTTPException(status_code=400, detail="Item already exists")
+    fake_db[item.id] = item
+    return item
