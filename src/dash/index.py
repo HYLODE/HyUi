41ec5c896145c96a1b23e2dea9e1c9c917a3d195
sharris9 <s.harris8@nhs.net>
2022-05-17 00:17:47 +0100

    wip: docker containers not communicating

--- src/dash/index.py
+++ src/dash/index.py
@@ -1,33 +1,55 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
+from dash import dash_table as dt
+from dash import dcc, html
+import requests
+
 from dash import Dash, html, dcc
 import plotly.express as px
-import pandas as pd
+
+# API_URL = "http://uclvlddpragae07:8094/consultations_ed/"
+API_URL = "http://api:8000/consultations_ed/"
+
+def request_data(url: str) -> list:
+    """
+    requests.json() should return a list of dicts
+    """
+    try:
+        response = requests.get(url)
+        response.raise_for_status() # raises HTTP errors
+    except requests.exceptions.HTTPError as e:
+        print("!!! HTTP Error", e)
+    except requests.exceptions.RequestException as e:
+        print("!!! Catch-all Error", e)
+
+    try:
+        assert response.headers['Content-Type'] == 'application/json'
+    except:
+        raise ValueError('response is not JSON')
+    return response.json()
+
+
+api_json = request_data(API_URL)
+
+debug = html.Div([
+    html.P('Printing a simple table from the API'),
+    dt.DataTable(
+        id='api_table',
+        columns = [{"name": i, "id": i} for i in api_json[0].keys()],
+        data=api_json,
+        filter_action="native",
+        sort_action="native"
+
+        )
+])
+
 
 app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame(
-    {
-        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-        "Amount": [4, 1, 2, 2, 4, 5],
-        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
-    }
-)
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
 app.layout = html.Div(
     children=[
-        html.H1(children="Hello Dash"),
-        html.Div(
-            children="""
-        Dash: A web application framework for your data.
-    """
-        ),
-        dcc.Graph(id="example-graph", figure=fig),
+        debug
     ]
 )
 
