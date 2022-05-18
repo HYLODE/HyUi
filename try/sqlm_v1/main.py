dcbe198b5bd9ced797604d225f74853078049fce
Steve Harris <doc@steveharris.me>
2022-05-18 18:39:01 +0100

    formatting

--- try/sqlm_v1/main.py
+++ try/sqlm_v1/main.py
@@ -4,45 +4,46 @@ from datetime import datetime
 from pydantic import BaseSettings
 import arrow
 
+
 class Settings(BaseSettings):
     EMAP_DB_HOST: str
     UDS_PWD: str
 
 
 class Consultation_Type(SQLModel, table=True):
-    __table_args__ = {'schema': 'star'}
+    __table_args__ = {"schema": "star"}
     # metadata = MetaData(schema="star")
     consultation_type_id: Optional[int] = Field(default=None, primary_key=True)
-    #stored_from: datetime
-    #valid_from: datetime
+    # stored_from: datetime
+    # valid_from: datetime
     code: str
     name: str
 
-    
-    
+
 class Consultation_Request(SQLModel, table=True):
-    __table_args__ = {'schema': 'star'}
+    __table_args__ = {"schema": "star"}
     # metadata = MetaData(schema="star")
     consultation_request_id: Optional[int] = Field(default=None, primary_key=True)
-    #stored_from: datetime
+    # stored_from: datetime
     valid_from: datetime
     cancelled: bool
     closed_due_to_discharge: bool
-    #scheduled_datetime: datetime
-    #status_change_datetime: datetime
+    # scheduled_datetime: datetime
+    # status_change_datetime: datetime
     # TODO: better way to pass schema into foreign key?
     consultation_type_id: int
     # consultation_type_id: Optional[int] = Field(default=None, foreign_key="star.consultation_type.consultation_type_id")
     # consultation_type_id: List['Consultation_type'] = Relationship(back_populates="consultation_request")
-    #hospital_visit_id: int
-    
-    
-    
+    # hospital_visit_id: int
+
+
 settings = Settings()
 
-postgresql_url = f"postgresql://sharris9:{settings.UDS_PWD}@{settings.EMAP_DB_HOST}:5432/uds"
+postgresql_url = (
+    f"postgresql://sharris9:{settings.UDS_PWD}@{settings.EMAP_DB_HOST}:5432/uds"
+)
 
-engine = create_engine(postgresql_url, echo=True)   
+engine = create_engine(postgresql_url, echo=True)
 
 
 def select_ConsultationType():
@@ -51,24 +52,31 @@ def select_ConsultationType():
         results = session.exec(statement)
         for res in results:
             print(res)
-            
+
+
 def select_ConsultationRequests():
-    # use arrow but convert back to datetime 
+    # use arrow but convert back to datetime
     # by .datetime method so compatible with type definition
-    start_ts = arrow.utcnow().to('Europe/London').shift(hours=-24).datetime
+    start_ts = arrow.utcnow().to("Europe/London").shift(hours=-24).datetime
     with Session(engine) as session:
-        statement = (select(Consultation_Request, Consultation_Type)
-            .where(Consultation_Type.consultation_type_id == Consultation_Request.consultation_type_id)
+        statement = (
+            select(Consultation_Request, Consultation_Type)
+            .where(
+                Consultation_Type.consultation_type_id
+                == Consultation_Request.consultation_type_id
+            )
             .where(Consultation_Request.valid_from >= start_ts)
-            .limit(1))
+            .limit(1)
+        )
         results = session.exec(statement)
         for res in results:
             print(res)
-        
-            
+
+
 def main():
     # select_ConsultationType()
     select_ConsultationRequests()
-    
+
+
 if __name__ == "__main__":
     main()
--- try/sqlm_v2/main.py
+++ try/sqlm_v2/main.py
@@ -5,22 +5,23 @@ from datetime import datetime
 from pydantic import BaseSettings
 import arrow
 
+
 class Settings(BaseSettings):
     EMAP_DB_HOST: str
     UDS_PWD: str
 
-    
+
 class Consultation_Type_Base(SQLModel):
-    #stored_from: datetime
+    # stored_from: datetime
     valid_from: datetime
     code: str
     name: str
-    
+
 
 class Consultation_Type(Consultation_Type_Base, table=True):
-    __table_args__ = {'schema': 'star'}
+    __table_args__ = {"schema": "star"}
     consultation_type_id: Optional[int] = Field(default=None, primary_key=True)
-    
+
 
 class Consultation_Type_Read(Consultation_Type_Base):
     consultation_type_id: int
@@ -28,8 +29,11 @@ class Consultation_Type_Read(Consultation_Type_Base):
 
 settings = Settings()
 
-postgresql_url = f"postgresql://sharris9:{settings.UDS_PWD}@{settings.EMAP_DB_HOST}:5432/uds"
-engine = create_engine(postgresql_url, echo=True)   
+postgresql_url = (
+    f"postgresql://sharris9:{settings.UDS_PWD}@{settings.EMAP_DB_HOST}:5432/uds"
+)
+engine = create_engine(postgresql_url, echo=True)
+
 
 def select_ConsultationType():
     with Session(engine) as session:
@@ -37,10 +41,12 @@ def select_ConsultationType():
         results = session.exec(statement)
         for res in results:
             print(res)
-            
+
+
 def get_session():
     with Session(engine) as session:
         yield session
+
 
 app = FastAPI()
 
@@ -52,5 +58,7 @@ def read_consultation_types(
     offset: int = 0,
     limit: int = Query(default=10, lte=10),
 ):
-    consultation_types = session.exec(select(Consultation_Type).offset(offset).limit(limit)).all()
+    consultation_types = session.exec(
+        select(Consultation_Type).offset(offset).limit(limit)
+    ).all()
     return consultation_types
