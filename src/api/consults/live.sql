b83f8c4bbff0bd531a7ff74e3c103928813e38cb
Steve Harris <doc@steveharris.me>
2022-05-29 13:22:08 +0100

    query all consults now

--- src/api/consults/live.sql
+++ src/api/consults/live.sql
@@ -1,6 +1,6 @@
 -- src/api/consults/live.sql
 WITH 
-q1 AS (
+consults AS (
 -- get all details on consultation requests
 -- regardless of where the patient is now
 SELECT 
@@ -23,19 +23,21 @@ SELECT
 ,ct.name
 ,loc.admission_time
 ,loc.discharge_time
+,loc.location_string
 ,loc.name AS dept_name
 
 FROM star.consultation_request cr
 LEFT JOIN star.consultation_type ct ON cr.consultation_type_id = ct.consultation_type_id
 
-LEFT JOIN (
+INNER JOIN (
 	SELECT 
 	*
 	FROM 
 	star.location_visit lv
 	LEFT JOIN star.location lo ON lv.location_id = lo.location_id
 	LEFT JOIN star.department de ON lo.department_id = de.department_id
-	WHERE lv.admission_time >= NOW() - INTERVAL '24 HOURS'
+--	WHERE lv.admission_time >= NOW() - INTERVAL '24 HOURS'
+	WHERE lv.discharge_time IS NULL
 ) loc ON cr.hospital_visit_id = loc.hospital_visit_id
 
 
@@ -44,40 +46,44 @@ LEFT JOIN star.core_demographic cd ON hv.mrn_id = cd.mrn_id
 LEFT JOIN star.mrn ON cd.mrn_id = mrn.mrn_id
 
 WHERE 
-	loc.speciality = 'Accident & Emergency'
+--	loc.speciality = 'Accident & Emergency'
 --	AND 
---	cr.closed_due_to_discharge = false
---	AND
---	cr.cancelled = false
+	hv.patient_class IN ('INPATIENT', 'EMERGENCY') 
+	AND
+	cr.closed_due_to_discharge = false
+	AND
+	cr.cancelled = false
 ORDER BY 
 cr.consultation_request_id
 ),
-q2 AS (
+loc_now AS (
 -- now number these by descending location admit time
 -- so '1' represents the current location of that patient
 SELECT
  *
-,row_number() over (partition BY q1.hospital_visit_id ORDER BY q1.admission_time DESC) loc_i 
-FROM q1
+,row_number() over (partition BY consults.hospital_visit_id ORDER BY consults.admission_time DESC) loc_i 
+FROM consults
 )
 -- now return the consult and the most recent location
 SELECT 
 -- select only those fields you wish to model and validate
- q2.firstname
-,q2.lastname
-,q2.date_of_birth
-,q2.mrn
-,q2.nhs_number
-,q2.valid_from
-,q2.cancelled
-,q2.closed_due_to_discharge
-,q2.comments
-,q2.scheduled_datetime
-,q2.status_change_time
-,q2.name
-,q2.admission_time
-,q2.discharge_time
-,q2.dept_name
-FROM q2
+ loc_now.firstname
+,loc_now.lastname
+,loc_now.date_of_birth
+,loc_now.mrn
+,loc_now.nhs_number
+,loc_now.valid_from
+--,loc_now.cancelled
+--,loc_now.closed_due_to_discharge
+,loc_now.comments
+,loc_now.scheduled_datetime
+,loc_now.status_change_time
+,loc_now.name
+,loc_now.admission_time
+,loc_now.discharge_time
+,loc_now.dept_name
+,loc_now.location_string
+FROM loc_now
 WHERE loc_i = 1
- ;
+ORDER BY loc_now.mrn ASC
+;
