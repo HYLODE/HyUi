da209ad0112d015d84f329927263bef0d80ea46a
Steve Harris <doc@steveharris.me>
2022-06-21 00:41:42 +0100

    rebuilt using new multipage system

--- dev/steve/log.md
+++ dev/steve/log.md
@@ -1,9 +1,15 @@
 # Running notes log
 
+## 2022-06-20t22:36:16
+https://github.com/HYLODE/HyUi/issues/46
+updated dash to 2.5.1
+had to reinstall pandas and numpy by hand
+all tests passed so sofarsogood
+
+
 ## 2022-06-12t12:33:47
 trying to reconfigure structure for better docker
 using tests to check progress
-
 
 ## 2022-06-09t00:39:45#
 working on playwright testing
--- src/apps/pages/sitrep/sitrep.py
+++ src/apps/pages/sitrep/sitrep.py
@@ -1,25 +1,29 @@
-# src/apps/census/census.py
+# src/apps/consults/consults.py
 """
-sub-application for census
+sub-application for consults
 """
+
 
 import dash_bootstrap_components as dbc
 from dash import Input, Output, callback
 from dash import dash_table as dt
 from dash import dcc, html
+from dash import register_page, page_registry
 
-from api.census.model import CensusRead
-from ..index import header
+from api.sitrep.model import SitrepRead
 
 from config.settings import settings
 from utils.dash import df_from_store, get_results_response
+
+register_page(__name__)
+print(page_registry)
 
 # APP to define URL
 # maybe run by HyUi API backend or maybe external
 # e.g
 #
 # HyUi API backend ...
-# API_URL = f"{settings.API_URL}/census/"
+# API_URL = f"{settings.API_URL}/consults/"
 #
 # External (HySys) backend ..
 # then define locally here within *this* app
@@ -32,17 +36,16 @@ from utils.dash import df_from_store, get_results_response
 
 if settings.ENV == "prod":
     WARD = "T03"  # initial definition
-    # http://uclvlddpragae07:5006/emap/icu/census/T03/
-    API_URL = f"{settings.BASE_URL}:5006/emap/icu/census/{WARD}"
+    API_URL = f"{settings.BASE_URL}:5006/live/icu/{WARD}/ui"
 else:
-    API_URL = f"{settings.API_URL}/census/"
+    API_URL = f"{settings.API_URL}/sitrep/"
 
 REFRESH_INTERVAL = 30 * 60 * 1000  # milliseconds
 
 
 @callback(
-    Output("census_request_data", "data"),
-    Input("census_query-interval", "n_intervals"),
+    Output("sitrep_request_data", "data"),
+    Input("sitrep_query-interval", "n_intervals"),
 )
 def store_data(n_intervals: int) -> list:
     """
@@ -53,16 +56,16 @@ def store_data(n_intervals: int) -> list:
 
 
 @callback(
-    Output("census_simple_table", "children"),
-    Input("census_request_data", "data"),
+    Output("sitrep_simple_table", "children"),
+    Input("sitrep_request_data", "data"),
     # prevent_initial_call=True,
 )
 def gen_simple_table(data: dict):
-    df = df_from_store(data, CensusRead)
+    df = df_from_store(data, SitrepRead)
     # limit columns here
     return [
         dt.DataTable(
-            id="census_simple_table_contents",
+            id="sitrep_simple_table_contents",
             columns=[{"name": i, "id": i} for i in df.columns],
             data=df.to_dict("records"),
             filter_action="native",
@@ -71,43 +74,33 @@ def gen_simple_table(data: dict):
     ]
 
 
-card_table = dbc.Card(
+sitrep_table = dbc.Card(
     [
-        dbc.CardHeader(html.H6("Census details")),
+        dbc.CardHeader(html.H6("Sitrep details")),
         dbc.CardBody(
             [
                 dcc.Loading(
                     id="loading-1",
                     type="default",
-                    children=html.Div(id="census_simple_table"),
+                    children=html.Div(id="sitrep_simple_table"),
                 )
             ]
         ),
     ]
 )
 
-main = html.Div(
-    [
-        card_table,
-    ]
-)
-
-
 dash_only = html.Div(
     [
         dcc.Interval(
-            id="census_query-interval", interval=REFRESH_INTERVAL, n_intervals=0
+            id="sitrep_query-interval", interval=REFRESH_INTERVAL, n_intervals=0
         ),
-        dcc.Store(id="census_request_data"),
+        dcc.Store(id="sitrep_request_data"),
     ]
 )
 
-census = dbc.Container(
-    fluid=True,
-    className="dbc",
-    children=[
-        header,
-        main,
+layout = html.Div(
+    [
+        sitrep_table,
         dash_only,
-    ],
+    ]
 )
