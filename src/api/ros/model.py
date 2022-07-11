143e55429950427ff1c2e893499f67fdddf40196
Daniel Hanlon <daniel.hanlon96@gmail.com>
2022-07-11 16:02:19 +0100

    create ROS endpoint

---
+++ src/api/ros/model.py
@@ -0,0 +1,55 @@
+# src/api/ros/model.py
+"""
+
+"""
+
+from datetime import date, datetime
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
+from config.settings import settings  # type: ignore
+
+
+# define the data model that you're expecting from your query
+class RosBase(SQLModel):
+
+    date_of_birth: date
+    admission_time: datetime
+    firstname: str
+    lastname: str
+    name: str
+    live_mrn: str
+    hospital_visit_id: str
+    order_datetime: datetime
+    lab_result_id: str
+
+    # @validator("dob", pre=True)
+    # def convert_datetime_to_date(cls, v):
+    #     if isinstance(v, str):
+    #         try:
+    #             return arrow.get(v).date()
+    #         except Exception as e:
+    #             print("Unable to convert dob to date")
+    #             print(e)
+    #     return v
+
+
+class RosMock(RosBase, table=True):
+    """
+    The table version of the pydantic class
+    Used for creating tables via SQLModel
+    """
+
+    # only set schema if in postgres
+    if "postgres" in settings.DB_URL:
+        __table_args__ = {"schema": settings.DB_POSTGRES_SCHEMA}
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class RosRead(RosBase):
+    """
+    Read version that includes the key column
+    """
+
+    id: Optional[int]
