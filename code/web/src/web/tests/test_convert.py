ee49a126bcc1206ba8c00cf9feda593b2316062a
Jon Gillham <jonathan.gillham@gmail.com>
2022-10-28 11:10:26 +0100

    Fix test

--- code/web/src/web/tests/test_convert.py
+++ code/web/src/web/tests/test_convert.py
@@ -9,12 +9,18 @@ class MyModel(BaseModel):
 
 
 def test_to_data_frame():
-    df = to_data_frame([MyModel(a=1, b="1"), MyModel(a=2, b="2")])
+    df = to_data_frame([MyModel(a=1, b="1"), MyModel(a=2, b="2")], MyModel)
     assert len(df.index) == 2
     assert df.iloc[0].a == 1
     assert df.iloc[0].b == "1"
     assert df.iloc[1].a == 2
     assert df.iloc[1].b == "2"
+
+
+def test_to_data_frame_empty_list():
+    df = to_data_frame([], MyModel)
+    assert len(df.index) == 0
+    assert df.columns.tolist() == ["a", "b"]
 
 
 def test_parse_to_data_frame():
