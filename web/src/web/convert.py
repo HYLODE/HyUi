30977c094db09036a0bb66bea75c263f4b107e72
Jon Gillham <jonathan.gillham@gmail.com>
2022-11-15 14:33:14 +0000

    Fix typing

--- web/src/web/convert.py
+++ web/src/web/convert.py
@@ -12,6 +12,6 @@ def to_data_frame(
     )
 
 
-def parse_to_data_frame(rows: list[dict], model_type: type[BaseModel]):
+def parse_to_data_frame(rows: list[dict], model_type: type[BaseModel]) -> pd.DataFrame:
     model_rows = (model_type.parse_obj(row) for row in rows)
     return to_data_frame(model_rows, model_type)
