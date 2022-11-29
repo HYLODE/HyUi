d34c58ae7f6d7ceacf886b48a62dad0418ce224e
Steve Harris <git@steveharris.me>
2022-11-29 21:45:39 +0000

    added pyodbc and live query script

---
+++ api/src/api/demo/live.sql
@@ -0,0 +1,12 @@
+-- 2022-07-21
+-- Query by J Hunter
+-- see https://github.com/HYLODE/HyUi/issues/47#issuecomment-1160706270
+
+SELECT
+     pod.NAME AS pod_orc
+    ,c.OR_CASE_ID AS or_case_id
+    ,c.SURGERY_DATE AS SurgeryDateClarity
+FROM OR_CASE c
+INNER JOIN ZC_OR_POSTOP_DEST pod ON pod.POSTOP_DEST = c.POSTOP_DEST_C
+WHERE c.SURGERY_DATE >= GETDATE()
+AND c.SURGERY_DATE <= CONVERT(DATE,DATEADD(DAY, :days_ahead ,CURRENT_TIMESTAMP))
