9a695153a7c5d4bbbfc6028516bfba0ecf1d471d
Jonathan Gillham <jonathan.gillham@gmail.com>
2022-12-05 11:07:19 +0000

    Add NO_PROXY code

--- initialise/bin/run.sh
+++ initialise/bin/run.sh
@@ -21,4 +21,8 @@ PROJECT_DIR="${BIN_DIR%/*/*}"
 
 docker build -t "hyui-initialise" -f "${PROJECT_DIR}/docker/initialise/Dockerfile" .
 
-docker run --rm --env-file "${PROJECT_DIR}/.env" hyui-initialise
+# Used to ensure that we don't use the proxy server when running from the same
+# machine. We only use the first part of the domain name below.
+HOSTNAME="$(hostname)"
+
+docker run --rm --env NO_PROXY="${HOSTNAME%%.*}" --env-file "${PROJECT_DIR}/.env" hyui-initialise
