877c40ce7d4bc31669e64b59ccfe9b34e92942c4
Jonathan Gillham <jonathan.gillham@gmail.com>
2022-12-06 12:20:07 +0000

    Do not use future SQL conn

--- initialise/src/initialise/db.py
+++ initialise/src/initialise/db.py
@@ -9,10 +9,10 @@ from initialise.config import get_db_settings
 @lru_cache()
 def caboodle_engine() -> Engine:
     settings = get_db_settings()
-    return create_engine(settings.caboodle_dsn, echo=False, future=True)
+    return create_engine(settings.caboodle_dsn, echo=False, future=False)
 
 
 @lru_cache()
 def star_engine() -> Engine:
     settings = get_db_settings()
-    return create_engine(settings.star_dsn, echo=False, future=True)
+    return create_engine(settings.star_dsn, echo=False, future=False)
