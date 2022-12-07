2bf5985048e92736820802ab99724349cee465ac
hqva <vaidya@hrish.ee>
2022-12-07 15:59:42 +0000

    Edited electives tests.

--- api/src/api/tests/test_electives.py
+++ api/src/api/tests/test_electives.py
@@ -1,15 +1,20 @@
-from datetime import date, datetime
+# from datetime import date, datetime
 
-from api.convert import to_data_frame
-from api.electives.wrangle import prepare_electives
+# from api.convert import to_data_frame
+
+# from api.electives.wrangle import prepare_draft  # prepare_electives
 from api.main import app
 from fastapi.testclient import TestClient
 
 from models.electives import (
-    CaboodleCaseBooking,
-    ClarityPostopDestination,
-    CaboodlePreassessment,
-    ElectiveSurgCase,
+    # CaboodleCaseBooking,
+    # ClarityPostopDestination,
+    # CaboodlePreassessment,
+    # ElectiveSurgCase,
+    PreassessData,
+    SurgData,
+    MergedData,
+    LabData,
 )
 
 client = TestClient(app)
@@ -19,23 +24,23 @@ def test_get_mock_preassess():
     response = client.get("/mock/electives/preassessment")
     assert response.status_code == 200
 
-    rows = [CaboodlePreassessment.parse_obj(row) for row in response.json()]
+    rows = [PreassessData.parse_obj(row) for row in response.json()]
     assert len(rows) > 0
 
 
-def test_get_mock_pod():
-    response = client.get("/mock/electives/postop_destination")
-    assert response.status_code == 200
+# def test_get_mock_pod():
+#     response = client.get("/mock/electives/postop_destination")
+#     assert response.status_code == 200
 
-    rows = [ClarityPostopDestination.parse_obj(row) for row in response.json()]
-    assert len(rows) > 0
+#     rows = [ClarityPostopDestination.parse_obj(row) for row in response.json()]
+#     assert len(rows) > 0
 
 
 def test_get_mock_cases():
     response = client.get("/mock/electives/case_booking")
     assert response.status_code == 200
 
-    rows = [CaboodleCaseBooking.parse_obj(row) for row in response.json()]
+    rows = [SurgData.parse_obj(row) for row in response.json()]
     assert len(rows) > 0
 
 
@@ -43,5 +48,13 @@ def test_get_mock_electives():
     response = client.get("/mock/electives")
     assert response.status_code == 200
 
-    elective_rows = [ElectiveSurgCase.parse_obj(row) for row in response.json()]
+    elective_rows = [MergedData.parse_obj(row) for row in response.json()]
+    assert len(elective_rows) > 0
+
+
+def test_get_mock_labs():
+    response = client.get("/mock/labs")
+    assert response.status_code == 200
+
+    elective_rows = [LabData.parse_obj(row) for row in response.json()]
     assert len(elective_rows) > 0
