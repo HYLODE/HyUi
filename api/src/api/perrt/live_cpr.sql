1d5c9ec00276337beb1bcaeae5521fa433eafe5c
Steve Harris <git@steveharris.me>
2022-12-08 22:43:53 +0000

    added schema to query

--- api/src/api/perrt/live_cpr.sql
+++ api/src/api/perrt/live_cpr.sql
@@ -10,8 +10,8 @@ SELECT
 	,ad.closed_due_to_discharge
 	,ad.hospital_visit_id
 	,hv.encounter
-FROM advance_decision ad
-LEFT JOIN advance_decision_type adt ON ad.advance_decision_type_id = adt.advance_decision_type_id
-LEFT JOIN hospital_visit hv ON ad.hospital_visit_id = hv.hospital_visit_id
+FROM star.advance_decision ad
+LEFT JOIN star.advance_decision_type adt ON ad.advance_decision_type_id = adt.advance_decision_type_id
+LEFT JOIN star.hospital_visit hv ON ad.hospital_visit_id = hv.hospital_visit_id
 WHERE hv.encounter = ANY( :encounter_ids )
 ;
