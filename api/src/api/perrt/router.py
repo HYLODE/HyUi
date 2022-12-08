582d67f880185aa5acee23b48933ac371a5a25b0
Steve Harris <git@steveharris.me>
2022-12-08 22:11:37 +0000

    adding CPR and PERRT consult endpoints
    consults and cpr endpoints
    
    adding cpr
    
    typos

--- api/src/api/perrt/router.py
+++ api/src/api/perrt/router.py
@@ -14,11 +14,61 @@ from api.convert import parse_to_data_frame, to_data_frame
 from api.db import get_star_session
 from api.mock import _get_json_rows, _parse_query
 from api.perrt.wrangle import wrangle
-from models.perrt import EmapVitalsLong, EmapVitalsWide
+from models.perrt import EmapConsults, EmapCpr, EmapVitalsLong, EmapVitalsWide
 
 router = APIRouter(prefix="/perrt")
 mock_router = APIRouter(prefix="/perrt")
 _this_file = Path(__file__)
+
+
+@router.get("/cpr", response_model=list[EmapCpr])
+def get_emap_cpr(
+    session: Session = Depends(get_star_session),
+    encounter_ids: list[str] = Query(default=[]),
+):
+    """
+    Return advance decisions about CPR
+    :type session: Session object from sqlmodel
+    """
+    params = {"encounter_ids": encounter_ids}
+    res = _parse_query(_this_file, "live_cpr.sql", session, EmapCpr, params)
+    return res
+
+
+@mock_router.get("/cpr", response_model=list[EmapCpr])
+def get_mock_emap_cpr():
+    """
+    Return mock consults to PERRT or ICU
+    :return:
+    """
+    rows = _get_json_rows(_this_file, "mock_cpr.json")
+    return rows
+
+
+@router.get("/consults", response_model=list[EmapConsults])
+def get_emap_perrt_consults(
+    session: Session = Depends(get_star_session),
+    encounter_ids: list[str] = Query(default=[]),
+    horizon_dt: dt.datetime = dt.datetime.now() - dt.timedelta(days=7),
+):
+    """
+    Return consults to PERRT or ICU
+    :type session: Session object from sqlmodel
+    :type horizon_dt: datetime remember to diff this from 'now'
+    """
+    params = {"encounter_ids": encounter_ids, "horizon_dt": horizon_dt}
+    res = _parse_query(_this_file, "live_consults.sql", session, EmapConsults, params)
+    return res
+
+
+@mock_router.get("/consults", response_model=list[EmapConsults])
+def get_mock_emap_perrt_consults():
+    """
+    Return mock consults to PERRT or ICU
+    :return:
+    """
+    rows = _get_json_rows(_this_file, "mock_consults.json")
+    return rows
 
 
 @router.get("/vitals/long", response_model=list[EmapVitalsLong])
