b4bdb8cba2c8a91ebf73b12c27b17477b448282f
hqva <vaidya@hrish.ee>
2022-12-09 12:57:16 +0000

    added sql scripts

--- api/src/api/electives/live_pod.sql
+++ api/src/api/electives/live_pod.sql
@@ -1,15 +1,15 @@
 -- noinspection SqlNoDataSourceInspectionForFile
-
 -- 2022-07-21
 -- Query by J Hunter
 -- see https://github.com/HYLODE/HyUi/issues/47#issuecomment-1160706270
-
 -- TODO: parameterise where clause for greater number of days
-SELECT
-     pod.NAME AS PodOrc
-    ,c.OR_CASE_ID AS OrCaseId
-    ,c.SURGERY_DATE AS SurgeryDateClarity
+SELECT pod.NAME AS PodOrc,
+    c.OR_CASE_ID AS OrCaseId,
+    c.SURGERY_DATE AS SurgeryDateClarity
 FROM OR_CASE c
-INNER JOIN ZC_OR_POSTOP_DEST pod ON pod.POSTOP_DEST = c.POSTOP_DEST_C
+    INNER JOIN ZC_OR_POSTOP_DEST pod ON pod.POSTOP_DEST = c.POSTOP_DEST_C
 WHERE c.SURGERY_DATE >= GETDATE()
-AND c.SURGERY_DATE <= CONVERT(DATE,DATEADD(DAY, :days_ahead ,CURRENT_TIMESTAMP))
+    AND c.SURGERY_DATE <= CONVERT(
+        DATE,
+        DATEADD(DAY, :days_ahead, CURRENT_TIMESTAMP)
+    )
