7271a4db3e398da7754d1e509a0eec948ae302c7
Jonathan Gillham <jonathan.gillham@gmail.com>
2022-12-15 17:09:30 +0000

    Deduplicate code

--- api/src/api/beds/router.py
+++ api/src/api/beds/router.py
@@ -40,3 +40,31 @@ def get_beds(department: str, settings: Settings = Depends(get_settings)) -> lis
 
     rows = get_rows(baserow_url, email, password, "hyui", "beds", params)
     return [Bed.parse_obj(row) for row in rows]
+
+
+@mock_router.get("/closed/", response_model=list[Bed])
+def get_mock_closed_beds() -> list[Bed]:
+    return [
+        Bed(location_string="LOC1", room="ROOM1", closed=True, covid=True),
+        Bed(location_string="LOC1", room="ROOM1", closed=True, covid=False),
+    ]
+
+
+@router.get("/beds/closed/", response_model=list[Bed])
+def get_closed_beds(settings=Depends(get_settings)) -> list[Bed]:
+    baserow_url = settings.baserow_url
+    email = settings.baserow_email
+    password = settings.baserow_password.get_secret_value()
+
+    field_ids = get_fields(baserow_url, email, password, "hyui", "beds")
+
+    closed_field_id = field_ids["closed"]
+
+    params = {
+        "size": 200,  # The maximum size of a page.
+        "user_field_names": "true",
+        f"filter__field_{closed_field_id}__boolean": True,
+    }
+
+    rows = get_rows(baserow_url, email, password, "hyui", "beds", params)
+    return [Bed.parse_obj(row) for row in rows]
