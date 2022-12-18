2042c5129e0db4d69ee4b298b5b2123252dc4c74
Steve Harris <git@steveharris.me>
2022-12-18 15:33:16 +0000

    fix #178

--- bin/initialise.sh
+++ bin/initialise.sh
@@ -10,7 +10,10 @@ fi
 BIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="${BIN_DIR%/*}"
 
-docker build -t "hyui-initialise" -f "${PROJECT_DIR}/docker/initialise/Dockerfile" .
+docker build -t "hyui-initialise" -f "${PROJECT_DIR}/docker/initialise/Dockerfile" \
+  --build-arg HTTP_PROXY --build-arg HTTPS_PROXY \
+  --build-arg http_proxy --build-arg https_proxy \
+  .
 
 # Used to ensure that we don't use the proxy server when running from the same
 # machine. We only use the first part of the domain name below.
