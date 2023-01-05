13b1faac600963bc3c37eeb298c79050d69a3c6c
Steve Harris <git@steveharris.me>
2023-01-05 09:44:17 +0000

    filter out non beds

--- web/src/web/pages/abacus/map.py
+++ web/src/web/pages/abacus/map.py
@@ -11,7 +11,6 @@ import dash_bootstrap_components as dbc
 import dash_cytoscape as cyto
 import math
 from dash import Input, Output, State, callback, ctx, dcc, html
-from datetime import datetime
 
 from web.pages.abacus import BPID
 
@@ -22,6 +21,15 @@ from web.pages.abacus.stores import (
     input_beds_data,
     input_census_data,
     input_sitrep_data,
+)
+from web.pages.abacus.map_utils import (
+    _make_bed,
+    _make_room,
+    _list_of_unique_rooms,
+    _populate_beds,
+    _update_discharges,
+    _update_patients_with_sitrep,
+    _filter_non_beds,
 )
 
 # NOTE: the following fails b/c circular so must fall back on string identifer
@@ -142,176 +150,12 @@ map_cyto_beds = dbc.Card(
                 stylesheet=_cyto_stylesheet,
                 responsive=True,
                 maxZoom=1.0,
-                minZoom=0.2,
+                zoomingEnabled=True,  # else won't 'fit'
+                minZoom=0.5,
             )
         ]
     )
 )
-
-
-def _as_int(s: int | float | None) -> int | None:
-    if s is None:
-        return None
-    else:
-        return int(float(s))
-
-
-def _populate_beds(bl: list[dict], cl: list[dict]) -> list[dict]:
-    """
-    Place patients in beds
-
-    Args:
-        bl: bed_list
-        cl: census_list
-
-    Returns:
-        a list of dictionaries to populate elements for Cytoscape
-    """
-    # convert to dictionary of dictionaries to search / merge
-    cd = {i["location_string"]: i for i in cl}
-    for bed in bl:
-        location_string = bed.get("data").get("id")  # type: ignore
-        census = cd.get(location_string, {})  # type: ignore
-        bed["data"]["census"] = census
-        bed["data"]["encounter"] = _as_int(census.get("encounter", None))
-        bed["data"]["occupied"] = True if census.get("occupied", None) else False
-    return bl
-
-
-def _update_patients_with_sitrep(bl: list[dict], sl: list[dict]) -> list[dict]:
-    """
-    Merge sitlist details on to patient list using encounter key
-    Args:
-        bl: beds with patients
-        sl: sitlist
-
-    Returns:
-        updated bed_list holding sitlist data
-
-    """
-
-    # convert to dictionary for searching simply
-    sd = {int(i["csn"]): i for i in sl if i["csn"]}
-    for bed in bl:
-        csn = bed.get("data").get("encounter")
-        sitrep = sd.get(csn, {})
-        bed["data"]["sitlist"] = sitrep
-        bed["data"]["wim"] = sitrep.get("wim_1")
-    return bl
-
-
-def _update_discharges(bl: list[dict], discharges: list[dict]) -> list[dict]:
-    # convert to dictionary of dictionaries to search / merge
-    dd = {i["csn"]: i for i in discharges}
-    for bed in bl:
-        csn = bed.get("data").get("encounter")
-        # if there's a patient in the bed
-        if not csn:
-            continue
-        # pm = planned_move
-        pm_type = bed["data"]["census"].get("pm_type")
-        pm_modified = bed["data"]["census"].get("pm_datetime")
-        bed["data"]["discharge"] = True if pm_type == "OUTBOUND" else False
-
-        # if there's better info on discharge status
-        dc_status = dd.get(csn, {})
-        bed["data"]["dc_status"] = dc_status
-        if not len(dc_status):
-            continue
-        dc_modified = dc_status.get("modified_at")
-        if not pm_modified:
-            bed["data"]["discharge"] = (
-                True if dc_status.get("status") == "ready" else False
-            )
-        else:
-            dc_modified = datetime.fromisoformat(dc_modified)
-            pm_modified = datetime.fromisoformat(pm_modified)
-            if dc_modified > pm_modified:
-                bed["data"]["discharge"] = (
-                    True if dc_status.get("status") == "ready" else False
-                )
-
-    return bl
-
-
-def _list_of_unique_rooms(beds: list) -> list:
-    rooms = [_split_loc_str(bed["location_string"], "room") for bed in beds]
-    rooms = list(set(rooms))
-    return rooms
-
-
-def _split_loc_str(s: str, part: str) -> str:
-    dept, room, bed = s.split("^")
-    if part == "dept":
-        return dept
-    elif part == "room":
-        return room
-    elif part == "bed":
-        return bed
-    else:
-        raise ValueError(f"{part} not one of dept/room/bed")
-
-
-def _make_bed(
-    bed: dict, scale: int = 9, offset: tuple[int, int] = (-100, -100)
-) -> dict:
-    position = {}
-    if bed.get("xpos") and bed.get("ypos"):
-        position.update(x=bed.get("xpos") * scale + offset[0]),  # type: ignore
-        position.update(y=bed.get("ypos") * scale + offset[0]),  # type: ignore
-
-    hl7_bed = _split_loc_str(bed["location_string"], "bed")
-    room = _split_loc_str(bed["location_string"], "room")
-
-    try:
-        pretty_bed = hl7_bed.split("-")[1]
-        try:
-            bed_index = int(pretty_bed)
-        except ValueError:
-            bed_index = 0
-    except IndexError:
-        pretty_bed = hl7_bed
-        bed_index = 0
-
-    data = dict(
-        id=bed["location_string"],
-        bed_index=bed_index,  # noqa
-        label=pretty_bed,
-        parent=room,
-        level="bed",
-        closed=bed.get("closed", False),
-        covid=bed.get("covid", False),
-    )
-
-    return dict(data=data, position=position)
-
-
-def _make_room(room: str) -> dict:
-    sideroom = False
-    try:
-        pretty_room = room.split(" ")[1]
-        room_type = pretty_room[:2]
-        room_number = pretty_room[2:]
-        if room_type == "BY":
-            label = "Bay "
-        elif room_type == "SR":
-            label = "Room "
-            sideroom = True
-        else:
-            label = ""
-        # noinspection PyAugmentAssignment
-        label = label + room_number
-    except IndexError:
-        label = room
-
-    return dict(
-        data=dict(
-            id=room,
-            label=label,
-            sideroom=sideroom,
-            level="room",
-        )
-    )
 
 
 @callback(
@@ -437,7 +281,7 @@ def _update_elements_for_layout(
 def make_cytoscape_elements(census, beds, sitrep, discharges):
     room_list = [_make_room(r) for r in _list_of_unique_rooms(beds)]
 
-    bed_list = [_make_bed(i) for i in beds]
+    bed_list = [_make_bed(i) for i in _filter_non_beds(beds)]
     bed_list.sort(key=lambda bed: bed.get("data").get("bed_index"))
     bed_list = _populate_beds(bed_list, census)
     bed_list = _update_discharges(bed_list, discharges)
