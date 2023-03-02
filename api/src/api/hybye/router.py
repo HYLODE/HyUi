0e5d605d918c8d0588f0ab7225ed1b7a93ed6ca4
Lawrence Adams <lawrencetadams@googlemail.com>
2023-03-02 15:30:31 +0000

    Properly type object to keep mypy happy

--- api/src/api/hybye/router.py
+++ api/src/api/hybye/router.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import random
+from typing import List
 
 from fastapi import APIRouter, Depends, Response
 from sqlalchemy import text
@@ -18,17 +19,17 @@ def get_mock_discharges_for_last_n_days(number_of_days: int) -> list[int]:
     return [random.randint(2, 200) for num in range(number_of_days)]
 
 
-@router.get("/discharge/n_days/{number_of_days}", response_model=list[DischargeRow])
+@router.get("/discharge/n_days/{number_of_days}", response_model=List[DischargeRow])
 def get_discharges_for_last_n_days(
     number_of_days: int,
     response: Response,
     session: Session = Depends(get_star_session),
-) -> [DischargeRow]:
+) -> List[DischargeRow]:
     response.headers["Cache-Control"] = "public, max-age=300"
     query = text((Path(__file__).parent / "get_inpatient_discharges.sql").read_text())
     result = session.execute(query, {"days": number_of_days})
 
-    discharge_rows: [DischargeRow] = []
+    discharge_rows: List[DischargeRow] = []
 
     for row in result:
         discharge_row = DischargeRow.parse_obj(row)
