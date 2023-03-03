99e8201702cc38b1c1b858d094f2a2d82cc6c066
Lawrence Adams <lawrencetadams@googlemail.com>
2023-03-03 07:52:00 +0000

    Improve discharge api testing

--- api/src/api/tests/test_hybye.py
+++ api/src/api/tests/test_hybye.py
@@ -6,10 +6,11 @@ from models.hybye import DischargeRow
 client = TestClient(app)
 
 
-def test_get_mock_discharges_for_last_n_days():
+def test_get_mock_discharges_for_last_n_days() -> None:
     response = client.get("/mock/hybye/discharge/n_days/28")
     assert response.status_code == 200
 
-    rendered_response = [DischargeRow.parse_obj(row) for row in response.json()]
-
     assert len(response.json()) == 28
+
+    rendered_response = [DischargeRow.parse_obj(row) for row in response.json()]
+    assert rendered_response is not None
