e1c04a918b07cbc70c0d7c9b6586b960bc494d9e
Lawrence Adams <lawrencetadams@googlemail.com>
2023-03-03 10:35:06 +0000

    Improve discharges mock test

--- api/src/api/tests/test_hybye.py
+++ api/src/api/tests/test_hybye.py
@@ -7,10 +7,17 @@ client = TestClient(app)
 
 
 def test_get_mock_discharges_for_last_n_days() -> None:
-    response = client.get("/mock/hybye/discharge/n_days/28")
+    response = client.get("/mock/hybye/discharge/n_days/28?campuses=UCH")
     assert response.status_code == 200
 
     assert len(response.json()) == 28
 
     rendered_response = [DischargeRow.parse_obj(row) for row in response.json()]
     assert rendered_response is not None
+
+
+def test_mock_behaviour_is_valid() -> None:
+    response = client.get("/mock/hybye/discharge/n_days/28")
+    assert response.status_code == 200
+
+    assert len(response.json()) == 0
