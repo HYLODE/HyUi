b9d8f270e4e0f3925ce750dbcc0307ac5b594986
Lawrence Adams <lawrencetadams@googlemail.com>
2023-03-04 13:17:03 +0000

    Handle None cases, add response model attribute

--- models/src/models/hybye.py
+++ models/src/models/hybye.py
@@ -12,16 +12,16 @@ class CensusData(BaseModel):
     location_id: int
     department: str
     location_string: str
-    ovl_admission: datetime
-    ovl_hv_id: int
-    open_visits_n: int
-    cvl_admission: datetime
-    cvl_discharge: datetime
-    cvl_hv_id: int
+    ovl_admission: datetime | None
+    ovl_hv_id: int | None
+    open_visits_n: int | None
+    cvl_admission: datetime | None
+    cvl_discharge: datetime | None
+    cvl_hv_id: int | None
     ovl_ghost: bool
     occupied: bool
-    patient_class: str
-    mrn: str
-    lastname: str
-    firstname: str
-    date_of_birth: datetime
+    patient_class: str | None
+    mrn: str | None
+    lastname: str | None
+    firstname: str | None
+    date_of_birth: datetime | None
