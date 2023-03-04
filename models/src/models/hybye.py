7ee1fca53616435aad4f88297031fe2c14af9b60
Lawrence Adams <lawrencetadams@googlemail.com>
2023-03-04 13:05:49 +0000

    Start Census Work
    Get current UCH inpatients (to expand to other sites in future)
    Setup router
    First sql attempt →

--- models/src/models/hybye.py
+++ models/src/models/hybye.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 
 from pydantic import BaseModel
 
@@ -6,3 +6,22 @@ from pydantic import BaseModel
 class HospitalFlowRow(BaseModel):
     event_date: date
     count: int
+
+
+class CensusData(BaseModel):
+    location_id: int
+    department: str
+    location_string: str
+    ovl_admission: datetime
+    ovl_hv_id: int
+    open_visits_n: int
+    cvl_admission: datetime
+    cvl_discharge: datetime
+    cvl_hv_id: int
+    ovl_ghost: bool
+    occupied: bool
+    patient_class: str
+    mrn: str
+    lastname: str
+    firstname: str
+    date_of_birth: datetime
