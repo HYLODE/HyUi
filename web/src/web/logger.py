dad361e50e313ab3913db4a6b45265783f1313b4
Steve Harris <git@steveharris.me>
2023-03-12 23:29:23 +0000

    created timing decorator for logs

--- web/src/web/logger.py
+++ web/src/web/logger.py
@@ -1,13 +1,58 @@
+import functools
 import notifiers
 from loguru import logger
 from notifiers.logging import NotificationHandler
 from pathlib import Path
 from web.config import get_settings
 from datetime import datetime
+import time
+from collections.abc import Callable
+from typing import TypeVar, ParamSpec
+
+# Strategy to manage type checking
+# https://stackoverflow.com/a/65602590/992999
+T = TypeVar("T")
+P = ParamSpec("P")
+
+
+def logger_timeit(*, level: str = "DEBUG") -> Callable:
+    """
+    Decorator to log timings
+    via https://loguru.readthedocs.io/en/stable/resources/recipes.html
+    #logging-entry-and-exit-of-functions-with-a-decorator
+
+
+    """
+
+    def wrapper(func: Callable[P, T]) -> Callable[P, T]:
+        name = func.__name__
+
+        @functools.wraps(func)
+        def wrapped(*args: P.args, **kwargs: P.kwargs) -> T:
+            logger_ = logger.opt(depth=1)
+            start = time.time()
+            result = func(*args, **kwargs)
+            end = time.time()
+            logger_.log(
+                level, f"Function {name} returned in" f" {1000 * (end - start):.1f}ms"
+            )
+            return result
+
+        return wrapped
+
+    return wrapper
+
 
 # Log file
 log_file = Path(__file__).parent / "web.log"
-logger.add(log_file, rotation="5MB", retention="30 days", level="INFO")
+logger.add(
+    log_file,
+    rotation="5MB",
+    retention="30 days",
+    level="INFO",
+    backtrace=True,
+    diagnose=True,
+)
 
 # Slack notifications
 slack_settings = {"webhook_url": get_settings().slack_log_webhook.get_secret_value()}
