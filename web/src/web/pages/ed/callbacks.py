17864e3659b10322b80a76926c4bfbdfcbf8078b
Harry Moss <h.moss@ucl.ac.uk>
2023-05-24 15:51:20 +0100

    Use old ed pages with slight cleanup

--- web/src/web/pages/ed/callbacks.py
+++ web/src/web/pages/ed/callbacks.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Any, Dict, List
 
 from dash import Input, Output, callback
 
@@ -87,11 +87,9 @@ def _get_aggregate_patients() -> list[AggregateAdmissionRow]:
     Output(ids.AGGREGATE_STORE, "data"),
     Input(app_ids.STORE_TIMER_15M, "n_intervals"),
 )
-def store_aggregate_patients(n_intervals: int) -> Optional[List[AggregateAdmissionRow]]:
+def store_aggregate_patients(n_intervals: int) -> List[Dict[str, Any]]:
     if n_intervals >= 0:
-        return _get_aggregate_patients()  # type: ignore
-    else:
-        return None
+        return _get_aggregate_patients()
 
 
 @logger_timeit()
@@ -105,13 +103,9 @@ def _get_individual_patients() -> list[EmergencyDepartmentPatient]:
     Output(ids.PATIENTS_STORE, "data"),
     Input(app_ids.STORE_TIMER_15M, "n_intervals"),
 )
-def store_individual_patients(
-    n_intervals: int,
-) -> Optional[List[EmergencyDepartmentPatient]]:
+def store_individual_patients(n_intervals: int) -> List[Dict[str, Any]]:
     if n_intervals >= 0:
-        return _get_individual_patients()  # type: ignore
-    else:
-        return None
+        return _get_individual_patients()
 
 
 @callback(
@@ -119,7 +113,7 @@ def store_individual_patients(
     Output(ids.PATIENTS_GRID, "columnDefs"),
     Input(ids.PATIENTS_STORE, "data"),
 )
-def build_patients_grid(data: list[dict]) -> tuple[list[dict], list[dict]]:
+def build_patients_grid(data):
     df = parse_to_data_frame(data, EmergencyDepartmentPatient)
     columnDefs = columnDefs_patients
-    return df.to_dict("records"), columnDefs  # type: ignore
+    return df.to_dict("records"), columnDefs
--- web/src/web/pages/ed/ed.py
+++ web/src/web/pages/ed/ed.py
@@ -3,29 +3,15 @@ import dash_ag_grid as dag
 import dash_mantine_components as dmc
 from dash import dcc, html
 
+dash.register_page(__name__, path="/ed/table", name="ED")
+
+
 from web.logger import logger
 from web.pages.ed import ids
 
-
-dash.register_page(__name__, path="/ed/table", name="ED")
+from web.pages.ed import callbacks
 
 logger.debug("Confirm that you have imported all the callbacks")
-
-# TODO: not sure that progress bar is the correct design since you don't know the 'max'
-# progress = dmc.Stack([
-#     dmc.Progress(
-#         id=ids.PROGRESS_MED,
-#         size="xl",
-#         sections=[
-#             {"label": "Allocated", "value": 3, "color": colors.green},
-#             {"label": "Requested", "value": 2, "color": colors.red},
-#             {"label": "Probable here", "value": 1, "color": colors.orange},
-#             {"label": "Probable YTA", "value": 1, "color": colors.yellow},
-#         ]
-#     ),
-#     ],
-#     align="left"
-# )
 
 grid = dag.AgGrid(
     id=ids.PATIENTS_GRID,
@@ -77,129 +63,3 @@ def layout() -> dash.html.Div:
             # inspector,
         ]
     )
-
-
-# @logger_timeit()
-# def _get_aggregations() -> list[AggregateAdmissionRow]:
-#     # response = requests.get(f"{get_settings().api_url}/ed/aggregate/")
-#     url = f"{get_settings().api_url}/ed/aggregate/"
-#     data = requests_try_cache(url)
-#     return [AggregateAdmissionRow.parse_obj(row) for row in data]
-
-
-# def _prettify_datetime(s: datetime) -> str:
-#     """Private method to format string"""
-#     return s.strftime("%H:%M %a %d %b")
-
-
-# @callback(
-#     Output("individual-predictions-table", "data"),
-#     Input("title", "children"),
-#     background=True,
-# )
-# def individual_predictions_table(title: Any) -> List[Dict]:
-#     patients = _get_individual_patients()
-#     ps = [patient.dict() for patient in patients]
-#     ps = sorted(ps, key=lambda i: i["arrival_datetime"], reverse=True)  # type: ignore
-#     for i, p in enumerate(ps):
-#         p["arrival_datetime_pretty"] = _prettify_datetime(p["arrival_datetime"])
-#         p["arrival_order"] = i + 1  # most recent patient = 1
-#     return ps
-
-
-# @callback(
-#     Output("beds-required", "data"),
-#     Input("title", "children"),
-#     background=True,
-# )
-# def beds_required(title: Any) -> Any:
-#     aggregations = _get_aggregations()
-#     return [aggregation.dict() for aggregation in aggregations]
-
-
-# def layout() -> dbc.Container:
-#     return dbc.Container(
-#         [
-#             dbc.Row(
-#                 dbc.Col(
-#                     html.H1(
-#                         id="title",
-#                         children="A&E Admission Predictions",
-#                     )
-#                 )
-#             ),
-#             dbc.Row(dbc.Col(html.H2("Aggregate Predictions"))),
-#             dbc.Row(
-#                 dbc.Col(
-#                     """
-#             Of the current patients in ED that do not have decisions to admit we
-#             predict the following numbers of beds will be required:
-#             """
-#                 )
-#             ),
-#             dbc.Row(
-#                 dbc.Col(
-#                     DataTable(
-#                         id="beds-required",
-#                         data=[],
-#                         columns=[
-#                             {"name": "Speciality", "id": "speciality"},
-#                             {
-#                                 "name": "Beds Required >90% Confidence",
-#                                 "id": "without_decision_to_admit_ninety_percent",
-#                             },
-#                             {
-#                                 "name": "Beds Required >70% Confidence",
-#                                 "id": "without_decision_to_admit_seventy_percent",
-#                             },
-#                         ],
-#                     )
-#                 )
-#             ),
-#             dbc.Row(dbc.Col(html.H2("Individual Predictions"))),
-#             dbc.Row(
-#                 dbc.Col(
-#                     DataTable(
-#                         id="individual-predictions-table",
-#                         data=[],
-#                         columns=[
-#                             {"name": "Reverse Order", "id": "arrival_order"},
-#                             {"name": "Arrival Date", "id": "arrival_datetime_pretty"},
-#                             {"name": "Bed", "id": "bed"},
-#                             {"name": "MRN", "id": "mrn"},
-#                             {"name": "Name", "id": "name"},
-#                             {"name": "Sex", "id": "sex"},
-#                             {"name": "Date Of Birth", "id": "date_of_birth"},
-#                             {
-#                                 "name": "Admission Likelihood",
-#                                 "id": "admission_probability",
-#                                 "type": "numeric",
-#                                 "format": FormatTemplate.percentage(0),
-#                             },
-#                             {
-#                                 "name": "Next Location",
-#                                 "id": "next_location",
-#                             },
-#                         ],
-#                         sort_action="native",
-#                         sort_mode="multi",
-#                         style_data_conditional=[
-#                             {
-#                                 "if": {
-#                                     "column_id": "prediction_as_real",
-#                                     "filter_query": (
-#                                         f"{{prediction_as_real}} >= {c / 10} "
-#                                         f"&& {{prediction_as_real}} < {c / 10 + 0.1}"
-#                                     ),
-#                                 },
-#                                 "backgroundColor": f"rgba(255, 65, 54, {c / 10})",
-#                             }
-#                             for c in range(0, 11)
-#                         ],
-#                     )
-#                 )
-#             ),
-#             dcc.Store(id="individual-data"),
-#         ],
-#         fluid=True,
-#     )
