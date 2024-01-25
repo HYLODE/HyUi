d886ba93b40a33727a9ec2c2231a92dafe017e3f
Harry Moss <h.moss@ucl.ac.uk>
2024-01-25 13:16:22 +0000

    Add test for NaT issue in census/wrangle.py

--- api/src/api/tests/test_census.py
+++ api/src/api/tests/test_census.py
@@ -3,7 +3,12 @@ import pandas as pd
 
 from api import wards
 from api.census.router import _fetch_mock_census
-from api.census.wrangle import aggregate_by_department
+from api.census.wrangle import (
+    aggregate_by_department,
+    _aggregate_by_department,
+    _split_location_string,
+    _remove_non_beds,
+)
 from api.main import app
 from fastapi.testclient import TestClient
 
@@ -22,6 +27,19 @@ def test_aggregate_by_department() -> None:
 
     census_df = pd.DataFrame((row.dict() for row in census_rows))
     aggregated_df = aggregate_by_department(census_df)
+
+    assert len(aggregated_df.index) > 0
+
+
+def test__aggregate_by_department_handles_nat() -> None:
+    census_rows = _fetch_mock_census(list(wards.ALL), [])
+
+    census_df = pd.DataFrame((row.dict() for row in census_rows))
+
+    census_df = _split_location_string(census_df)
+    census_df = _remove_non_beds(census_df)
+
+    aggregated_df = _aggregate_by_department(census_df)
 
     assert len(aggregated_df.index) > 0
 
